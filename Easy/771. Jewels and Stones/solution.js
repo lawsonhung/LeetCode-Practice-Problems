2abb03c395b3d747ab6fe12bd4be5052e201e8e6
lawsonhung <lawsonhung@yahoo.com>
2020-03-21 15:02:41 -0400

    771 solution

---
+++ Easy/771. Jewels and Stones/solution.js
@@ -0,0 +1,35 @@
+/**You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+ */
+
+ /**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+  let jewels = J.split("");
+  let stones = S.split("");
+  let ans = 0;
+  
+  for(let i = 0; i < jewels.length; i++){
+      for(let j = 0; j < stones.length; j++){
+          // if (jewels[i] === stones[j]){
+          //     ans++;
+          // }
+          (jewels[i] === stones[j]) ? ans++ : null;
+      }
+  }
+  
+  return ans;
+};
