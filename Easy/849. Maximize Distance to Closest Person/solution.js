56cb71ab77ec01ac67f8b5ae2184067c91f1b99b
lawsonhung <lawsonhung@yahoo.com>
2020-06-09 20:26:11 -0400

    Solution 849

--- Easy/849. Maximize Distance to Closest Person/solution.js
+++ Easy/849. Maximize Distance to Closest Person/solution.js
@@ -22,55 +22,120 @@ Explanation:
 If Alex sits in the last seat, the closest person is 3 seats away.
 This is the maximum distance possible, so the answer is 3. */
 
+// /**
+//  * @param {number[]} seats
+//  * @return {number}
+//  */
+// var maxDistToClosest = function(seats) {
+//   let maxOpenSeatCt = 0;
+//   let maxOpenSeatLastIndex = null;
+//   let prevTakenSeatIndex = null;
+//   let tempOpenSeatCt = 0;
+  
+// //  Assign maxOpenSeatLastIndex a value
+//   for (let i in seats) {
+//       if (seats[i] === 1)
+//           tempOpenSeatCt = 0;
+//       else {
+//           tempOpenSeatCt++;
+//           if (Math.max(maxOpenSeatCt, tempOpenSeatCt) >= maxOpenSeatCt) {
+//               maxOpenSeatCt = Math.max(maxOpenSeatCt, tempOpenSeatCt);
+//               maxOpenSeatLastIndex = i;
+//           }
+//       }
+//   }
+  
+//   console.log("maxOpenSeatCt: ", maxOpenSeatCt);
+//   console.log("maxOpenSeatLastIndex: ", maxOpenSeatLastIndex);
+  
+// //  Assign prevTakenSeatIndex a value
+//   prevTakenSeatIndex = maxOpenSeatLastIndex - maxOpenSeatCt;
+//   console.log("prevTakenSeatIndex: ", prevTakenSeatIndex);
+  
+// //  If all seats on the left are open, return the maxOpenSeatCt
+//   if (prevTakenSeatIndex === -1) {
+//       return maxOpenSeatCt;
+//   }
+  
+// //  If it's the last seat, return the difference between the last open seat index and the last taken seat index
+//   if (maxOpenSeatLastIndex == seats.length - 1) {
+//       console.log("This is the last seat in the row");
+//       return maxOpenSeatLastIndex - prevTakenSeatIndex;
+//   } else if (prevTakenSeatIndex == seats.length - 1) {
+//       console.log("Only the last seat in the row is taken");
+//   }else {
+// //      There is a next taken seat right after the maxOpenSeatLastIndex
+// //      Divide maxOpenSeatCt by 2 because Alex will be sitting in the middle
+// //      Round up for odd numbers
+//       // if (maxOpenSeatCt % 2 !== 0)
+//           return Math.ceil(maxOpenSeatCt / 2);
+// //      If even, divide by 2
+//       // else
+//       //     return maxOpenSeatCt / 2;
+//   }
+// };
+
 /**
  * @param {number[]} seats
  * @return {number}
  */
 var maxDistToClosest = function(seats) {
-  let maxOpenSeatCt = 0;
-  let maxOpenSeatLastIndex = null;
-  let prevTakenSeatIndex = null;
-  let tempOpenSeatCt = 0;
+  // //     Check if only last seat is taken
+  // //     [0,0,1]
+  // //     => 2
+  // //     Check if last seat is taken
+  //     if (seats[seats.length - 1] === 1) {
+          
+  // //         Check if any middle seat is taken
+  //         let allLeftSeatsOpen = false;
+  //         let firstTakenSeatIndex = null;
+  //         for (let i = 0; i < seats.length - 1; i++) {
+  //             if (seats[i] === 1) {
+  //                 allLeftSeatsOpen = !allLeftSeatsOpen;
+  //                 firstTakenSeatIndex = i;
+  //                 break;
+  //             }
+  //         }
+          
+  // //         Check if all other seats to the right are taken
+  // //         [0,0,1,1]
+  //         let allRightSeatsTaken = true;
+  //         for (let i = firstTakenSeatIndex; i < seats.length - 1; i++) {
+  //             // console.log(seats[i]);
+  //             if (seats[i] === 0) {
+  //                 allRightSeatsTaken != allRightSeatsTaken;
+  //                 break;
+  //             }
+  //         }
+          
+  //         if (allLeftSeatsOpen && allRightSeatsTaken) {
+  //             return firstTakenSeatIndex;
+  //         }
+  //     }
   
-//  Assign maxOpenSeatLastIndex a value
-  for (let i in seats) {
-      if (seats[i] === 1)
-          tempOpenSeatCt = 0;
-      else {
-          tempOpenSeatCt++;
-          if (Math.max(maxOpenSeatCt, tempOpenSeatCt) >= maxOpenSeatCt) {
-              maxOpenSeatCt = Math.max(maxOpenSeatCt, tempOpenSeatCt);
-              maxOpenSeatLastIndex = i;
-          }
+  // //     Check if only first seat is taken
+  // //     [1,0,0,0]
+  // //     => 3
+      
+  // //     An open seat is somewhere in the middle
+  // //     [1,0,0,0,1,0,1]
+  // //     => 2
+      
+      let maxSeatCt = -Infinity;
+      
+      for (let i = 0, j = 0; i < seats.length; i = j) {
+  //         Locate left border of 0 [i]
+          for (i = j; i < seats.length && seats[i] === 1; i++) {}
+          
+  //         Locate right border of 0 [j - 1]
+          for (j = i; j < seats.length && seats[j] === 0; j++) {}
+          
+          console.log(i, j);
+          if (i === 0 || j === seats.length)
+              maxSeatCt = Math.max(maxSeatCt, j - i);
+          else
+              maxSeatCt = Math.max(maxSeatCt, Math.floor((j - i + 1) / 2));
       }
-  }
-  
-  console.log("maxOpenSeatCt: ", maxOpenSeatCt);
-  console.log("maxOpenSeatLastIndex: ", maxOpenSeatLastIndex);
-  
-//  Assign prevTakenSeatIndex a value
-  prevTakenSeatIndex = maxOpenSeatLastIndex - maxOpenSeatCt;
-  console.log("prevTakenSeatIndex: ", prevTakenSeatIndex);
-  
-//  If all seats on the left are open, return the maxOpenSeatCt
-  if (prevTakenSeatIndex === -1) {
-      return maxOpenSeatCt;
-  }
-  
-//  If it's the last seat, return the difference between the last open seat index and the last taken seat index
-  if (maxOpenSeatLastIndex == seats.length - 1) {
-      console.log("This is the last seat in the row");
-      return maxOpenSeatLastIndex - prevTakenSeatIndex;
-  } else if (prevTakenSeatIndex == seats.length - 1) {
-      console.log("Only the last seat in the row is taken");
-  }else {
-//      There is a next taken seat right after the maxOpenSeatLastIndex
-//      Divide maxOpenSeatCt by 2 because Alex will be sitting in the middle
-//      Round up for odd numbers
-      // if (maxOpenSeatCt % 2 !== 0)
-          return Math.ceil(maxOpenSeatCt / 2);
-//      If even, divide by 2
-      // else
-      //     return maxOpenSeatCt / 2;
-  }
-};
+      
+      return maxSeatCt
+  };
