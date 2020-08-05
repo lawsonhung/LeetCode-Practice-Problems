57d41d2a606f28d1c81b3ce408cf03d5c40d0d04
lawsonhung <lawsonhung@yahoo.com>
2020-08-04 22:58:39 -0400

    Add pseudocode comments 509.

--- Easy/509. Fibonacci Number/solution.js
+++ Easy/509. Fibonacci Number/solution.js
@@ -17,7 +17,10 @@ Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1. */
  * @return {number}
  */
 var fib = function(N) {
-  if (N === 0) return 0;
-  else if (N === 1) return 1;
-  else return fib(N-1) + fib(N-2);
-};
+  //     If N === 0, return 0
+      if (N === 0) return 0;
+  //     If N === 1, return 1
+      else if (N === 1) return 1;
+  //     Recursive: return fib(N-1) + fib(N-2)
+      else return fib(N-1) + fib(N-2);
+  };
