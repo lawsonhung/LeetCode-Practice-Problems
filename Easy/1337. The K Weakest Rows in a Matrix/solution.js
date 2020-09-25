5b697d833654cad9ddbd3b12117c5cab24eee9f8
lawsonhung <lawsonhung@yahoo.com>
2020-09-24 23:53:26 -0400

    Solution 1337.

--- Easy/1337. The K Weakest Rows in a Matrix/solution.js
+++ Easy/1337. The K Weakest Rows in a Matrix/solution.js
@@ -23,38 +23,64 @@ row 3 -> 2
 row 4 -> 5 
 Rows ordered from the weakest to the strongest are [2,0,3,1,4]
  */
-
- /**
+/**
  * @param {number[][]} mat
  * @param {number} k
  * @return {number[]}
  */
 var kWeakestRows = function(mat, k) {
-  let reducedMat = [];
-  let ans = [];
-  
-//     Iterate through mat
-  for (let row of mat) {
-//     Iterate through mat[i] with reduce
-      const reducer = (accumulator, currentVal) => accumulator + currentVal;
-      const reducedVal = row.reduce(reducer);
-//     Store reduced value in new array
-      reducedMat.push(reducedVal);
-  }
-  
-  console.log(reducedMat);
-  let reducedMatSplice = reducedMat;
-//     Iterate through stored array to find weakest rows (lowest reduced value)
-  while (k > 0) {
-      const min = Math.min(...reducedMatSplice);
-//     Store lowest values in answer array
-      const i = reducedMat.indexOf(min);
-      ans.push(i);
-      const indexSplice = reducedMatSplice.indexOf(min);
-      reducedMatSplice.splice(indexSplice,1);
+  //     let reducedMat = [];
+  //     let ans = [];
       
-      k--;
-  }
-//     Return answer
-  return ans;
-};
+  // //     Iterate through mat
+  //     for (let row of mat) {
+  // //     Iterate through mat[i] with reduce
+  //         const reducer = (accumulator, currentVal) => accumulator + currentVal;
+  //         const reducedVal = row.reduce(reducer);
+  // //     Store reduced value in new array
+  //         reducedMat.push(reducedVal);
+  //     }
+      
+  //     console.log(reducedMat);
+  //     let reducedMatSplice = reducedMat;
+  // //     Iterate through stored array to find weakest rows (lowest reduced value)
+  //     while (k > 0) {
+  //         const min = Math.min(...reducedMatSplice);
+  // //     Store lowest values in answer array
+  //         const i = reducedMat.indexOf(min);
+  //         ans.push(i);
+  //         const indexSplice = reducedMatSplice.indexOf(min);
+  //         reducedMatSplice.splice(indexSplice,1);
+          
+  //         k--;
+  //     }
+  // //     Return answer
+  //     return ans;
+      //console.log(mat, k, '<---');    
+      let result = []; 
+      for (let i of mat){        
+          const counter = i.filter(j => j != 0);        
+          result.push(counter.length);    
+      };        
+  
+      let resultObj = Object.assign({}, result);        
+  
+      let sorted = [];    
+  
+      for (var el in resultObj) {    
+          sorted.push([el, resultObj[el]]);    
+      }    
+  
+      sorted.sort(function(a, b) {        
+          return a[1] - b[1];    
+      });        
+  
+      let finalResult = sorted.slice(0,k);    
+      let finalFinalResult = [];    
+      for (let elem of finalResult){        
+          finalFinalResult.push(elem[0]);    
+      }        
+  
+      return finalFinalResult;
+  };
+  
