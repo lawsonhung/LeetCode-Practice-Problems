53822fd5e12f1af70081a3c726fe6ba1b5ea0712
lawsonhung <lawsonhung@yahoo.com>
2021-10-10 21:29:16 -0400

    Solution 2006.

---
+++ Easy/2006. Count Number of Pairs With Absolute Difference K/solution.js
@@ -0,0 +1,36 @@
+/**Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+The value of |x| is defined as:
+
+x if x >= 0.
+-x if x < 0.
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,1], k = 1
+Output: 4
+Explanation: The pairs with an absolute difference of 1 are:
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1]
+- [1,2,2,1] */
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+ var countKDifference = function(nums, k) {
+  let ans = 0;
+  
+  for(let i = 0; i < nums.length; i++) {
+      for(let j = i; j < nums.length; j++) {
+          if (Math.abs(nums[i] - nums[j]) === k) {
+              ans++;
+          }
+      }
+  }
+  
+  return ans;
+};
